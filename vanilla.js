6e196dc30c7e05f7357aeb072cfdfcda50084d7d
Ali Ilman <aliilman532@gmail.com>
2017-07-31 16:26:40 +0800

    Progress Made
    More content, styling and JavaScript.

--- vanilla.js
+++ vanilla.js
@@ -1,12 +1,32 @@
 //Variables
-let navLinks = document.getElementById("links");
-let menuShadow = document.getElementById("menu-container");
-
+let landingContainer = document.getElementById("landing-container"),
+	menuContainer = document.getElementById("menu-container"),
+	menuBtn = document.getElementById("menu"),
+	navLinks = document.getElementById("links");
 //Functions
 function menuToggle() {
 	navLinks.classList.toggle("linksOnOff");
 }
 
+function menuShadow() {
+	//If page is past landing container-64.
+	if(document.body.scrollTop+48 > landingContainer.clientHeight) {
+		menuContainer.style.boxShadow = "0px 2px 1px rgba(51, 51, 51, 0.16)";
+	}
+	else {
+		menuContainer.style.boxShadow = "";
+	}
+}
+
+//onClick for menu button.
+menuBtn.onclick = menuToggle;
+
+//onScroll for menuContainer's box-shadow.
+window.onscroll = menuShadow;
+
+//Unused
+
+//Functions
 function nonMobileNavStyle() {
 	if (document.body.scrollTop > 64) {
 		navLinks.style.backgroundColor = "white";
@@ -26,3 +46,17 @@ function mobileNavStyle() {
 		document.getElementById("menu-container").style.backgroundColor = "";
 	}
 }
+
+//Telling the computer what to do.
+
+//An if/else statement to check if window's width is more than 768px. Do "that" if more, do "this" if less.
+/*if(winWidth >= 768) {
+	window.onscroll = nonMobileNavStyle;
+}*/
+//An if/else statement for #menu-container box-shadow.
+/*if (document.body.scrollTop > 64) {
+	menuShadow.style.boxShadow = "0px 2px 2px rgba(51, 51, 51, 0.16)";
+}
+else {
+	menuShadow.style.boxShadow = "0px 0px 2px rgba(51, 51, 51, 0.16)";
+}*/
