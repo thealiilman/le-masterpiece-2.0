fe71774ba85e8b99023914f465a824a088545f28
Ali Ilman <aliilman532@gmail.com>
2017-11-17 00:02:31 +0800

    Added AJAX to send email without reloading.

--- res/js/script.js
+++ res/js/script.js
@@ -1,39 +1,59 @@
-$(function() {
+$(function () {
 	let landingContainer = document.getElementById("landing-container"),
 		menuContainer = document.getElementById("menu-container");
 	function menuShadow() {
 		//If page is past landing container-64.
-		if(document.body.scrollTop+50 > landingContainer.clientHeight) {
+		if (document.body.scrollTop > landingContainer.clientHeight) {
 			menuContainer.style.boxShadow = "0px 2px 1px rgba(51, 51, 51, 0.16)";
 		}
 		else {
 			menuContainer.style.boxShadow = "";
 		}
 	}
-	window.onscroll = menuShadow;
-	
-	$("#menu").on("click", function() {
+	window.onscroll = menuShadow();
+
+	$("#menu").on("click", function () {
 		$("#links").toggleClass("linksOnOff");
 	});
-	
-	$("#links a").on("click", function() {
+
+	$("#links a").on("click", function () {
 		$("#links").removeClass("linksOnOff");
 	});
-	
-	$("#about").on("click", function(){
+
+	$("#about").on("click", function () {
 		$("html, body").animate({
-			scrollTop: $("#about-container").offset().top-48}, 2000);
+			scrollTop: $("#about-container").offset().top - 48
+		}, 2000);
 	});
-	$("#services").on("click", function(){
+	$("#services").on("click", function () {
 		$("html, body").animate({
-			scrollTop:$("#services-container").offset().top-48}, 2000);
+			scrollTop: $("#services-container").offset().top - 48
+		}, 2000);
 	});
-	$("#portfolio").on("click", function(){
+	$("#portfolio").on("click", function () {
 		$("html, body").animate({
-			scrollTop:$("#portfolio-container").offset().top-48}, 2000);
+			scrollTop: $("#portfolio-container").offset().top - 48
+		}, 2000);
 	});
-	$("#contact").on("click", function(){
+	$("#contact").on("click", function () {
 		$("html, body").animate({
-			scrollTop:$("#contact-container").offset().top-48}, 2000);
+			scrollTop: $("#contact-container").offset().top - 48
+		}, 2000);
 	});
+	$('#formbtn').click(function(event) {
+		event.preventDefault();
+    $.ajax({
+      url: 'form.php',
+      type: 'POST',
+      data: {
+        name: $("#form-name").val(),
+        email: $("#form-email").val(),
+        subject: $("#form-subject").val(),
+        message: $("#form-message").val()
+      },
+      success: function() {
+          alert('Email Sent');
+      }       
+    });
+  });
 });
