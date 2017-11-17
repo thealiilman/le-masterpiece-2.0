a598bfbb3b9904bdd7c8016616c7be0fde7e29b7
Ali Ilman <aliilman532@gmail.com>
2017-11-17 16:15:23 +0800

    Added send-email-notify

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function () {
 		menuContainer = document.getElementById("menu-container");
 	function menuShadow() {
 		//If page is past landing container-64.
-		if (document.body.scrollTop > landingContainer.clientHeight) {
+		if (document.body.scrollTop + 50 > landingContainer.clientHeight) {
 			menuContainer.style.boxShadow = "0px 2px 1px rgba(51, 51, 51, 0.16)";
 		}
 		else {
@@ -25,16 +25,19 @@ $(function () {
 			scrollTop: $("#about-container").offset().top - 48
 		}, 2000);
 	});
+
 	$("#services").on("click", function () {
 		$("html, body").animate({
 			scrollTop: $("#services-container").offset().top - 48
 		}, 2000);
 	});
+
 	$("#portfolio").on("click", function () {
 		$("html, body").animate({
 			scrollTop: $("#portfolio-container").offset().top - 48
 		}, 2000);
 	});
+
 	$("#contact").on("click", function () {
 		$("html, body").animate({
 			scrollTop: $("#contact-container").offset().top - 48
@@ -43,18 +46,31 @@ $(function () {
 	
 	$('#formbtn').click(function(event) {
 		event.preventDefault();
-    $.ajax({
-      url: "form.php",
-      type: "POST",
-      data: {
-        name: $("#form-name").val(),
-        email: $("#form-email").val(),
-        subject: $("#form-subject").val(),
-        message: $("#form-message").val()
-      },
-      success: function() {
-          alert("Email has been sent!");
-      }
-    });
+    var sendEmailNotify = document.getElementById("send-email-notify");
+		var name = document.getElementById("form-name").value;
+		var email = document.getElementById("form-email").value;
+		var subject = document.getElementById("form-subject").value;
+		var message = document.getElementById("form-message").value;
+		if(name === "" || email === "" || subject === "" || message === "") {
+      sendEmailNotify.classList.add("send-email-failure");
+			sendEmailNotify.textContent = "Please fill in the form";
+		}
+		else {
+			$.ajax({
+				url: 'form.php',
+				type: 'POST',
+				data: {
+					name: name,
+					email: email,
+					subject: subject,
+					message: message
+				},
+				success: function() {
+          sendEmailNotify.classList.remove("send-email-failure");
+					sendEmailNotify.classList.add("send-email-success");
+          sendEmailNotify.textContent = "Email has been sent! Ali will get back to you soon.";
+				}       
+			});
+		}
   });
 });
